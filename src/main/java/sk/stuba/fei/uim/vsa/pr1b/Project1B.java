d16b065fa72e25572f69c3738a3c3fb4a79b3237
simonkrupa <xkrupas@stuba.sk>
2022-04-06 23:32:03 +0200

    uprava returnov

--- src/main/java/sk/stuba/fei/uim/vsa/pr1b/Project1B.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr1b/Project1B.java
@@ -1,18 +1,11 @@
 package sk.stuba.fei.uim.vsa.pr1b;
 
-import sk.stuba.fei.uim.vsa.pr1b.entities.*;
 import sk.stuba.fei.uim.vsa.pr1b.ui.*;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class Project1B {
 
-    public static void main(String[] args) throws InterruptedException {
-//        CarParkService carParkService = new CarParkService();
+    public static void main(String[] args) {
         Repl repl = new Repl();
         repl.start();
     }
